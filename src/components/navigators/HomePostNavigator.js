68125224733d8b70c96758d4d5eab521def8ae8a
Julie Wang <juliewang617@gmail.com>
2024-08-06 19:31:20 -0400

    more styling improvements

--- src/components/navigators/HomePostNavigator.js
+++ src/components/navigators/HomePostNavigator.js
@@ -1,28 +1,36 @@
-import * as React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from '../../screens/HomeScreen';
-import UserContext from '../contexts/UserContext';
-import { useContext } from 'react';
-import PostScreen from '../../screens/PostScreen';
-import CommenterContext from '../contexts/CommenterContext';
-import OtherProfileScreen from '../../screens/OtherProfileScreen';
-import CatScreen from '../../screens/CatScreen';
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../../screens/HomeScreen";
+import UserContext from "../contexts/UserContext";
+import { useContext } from "react";
+import PostScreen from "../../screens/PostScreen";
+import CommenterContext from "../contexts/CommenterContext";
+import OtherProfileScreen from "../../screens/OtherProfileScreen";
+import CatScreen from "../../screens/CatScreen";
 
 const HomePostStack = createStackNavigator();
 
-export default function HomePostNavigator(){
+/**
+ * @returns The HomePost Navigator.
+ */
+export default function HomePostNavigator() {
+  const { username, pfp, uid } = useContext(UserContext);
 
-    const { username, pfp, uid } = useContext(UserContext);  
-
-    return(
-        <CommenterContext.Provider value={{commentUsername: username, commentPfp: pfp, commentUid: uid}}>
-            <HomePostStack.Navigator
-            screenOptions={{headerTitle: "", headerTransparent: true}}>
-                <HomePostStack.Screen name = "Feed" component={HomeScreen}/>
-                <HomePostStack.Screen name = "Post" component={PostScreen} />
-                <HomePostStack.Screen name = "Other Profile" component={OtherProfileScreen} />
-                <HomePostStack.Screen name = "Cat" component={CatScreen} />
-            </HomePostStack.Navigator>
-        </CommenterContext.Provider>
-    )
+  return (
+    <CommenterContext.Provider
+      value={{ commentUsername: username, commentPfp: pfp, commentUid: uid }}
+    >
+      <HomePostStack.Navigator
+        screenOptions={{ headerTitle: "", headerTransparent: true }}
+      >
+        <HomePostStack.Screen name="Feed" component={HomeScreen} />
+        <HomePostStack.Screen name="Post" component={PostScreen} />
+        <HomePostStack.Screen
+          name="Other Profile"
+          component={OtherProfileScreen}
+        />
+        <HomePostStack.Screen name="Cat" component={CatScreen} />
+      </HomePostStack.Navigator>
+    </CommenterContext.Provider>
+  );
 }
